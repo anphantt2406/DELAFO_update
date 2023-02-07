fa7adcc6184d23bd8726c6894379f9cc7f59f752
Thanh Dao <thanhdao@Thanhs-MacBook-Air.local>
2023-02-07 22:47:40 +0700

    remove print mean, std each k-fold

--- models/attention_layer.py
+++ models/attention_layer.py
@@ -11,11 +11,11 @@ class AdditiveAttentionLayer(Layer):
 		super(AdditiveAttentionLayer, self).__init__(**kwargs)
 
 	def get_config(self):
-	config = super(AdditiveAttentionLayer, self).get_config()
-	config.update({
-					"arg1": self.latent_dim,
-					"arg2": self.kernel_regularizer,})
-	return config
+		config = super(AdditiveAttentionLayer, self).get_config()
+		config.update({
+						"arg1": self.latent_dim,
+						"arg2": self.kernel_regularizer,})
+		return config
 
 	def build(self, input_shape):
 		in_seq_shape = input_shape[0]
