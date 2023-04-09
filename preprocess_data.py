1160d0960c06efaa9d7c7daff05f74c67379f5c9
Phan Thị Thùy An <73625290+anphantt2406@users.noreply.github.com>
2023-04-09 15:47:52 +0700

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -34,7 +34,7 @@ def calculateEma(series, period, keep_length= True):
         ema.append(tmp)
     return np.asarray(ema, dtype= np.float32)
 
-def prepair_data(path, marketcap, last_date, n,window_x,window_y, close= ['interpolate','ffill'], vol= ['interpolate','fill0']):
+def prepair_data(path, marketcap, last_date, n,window_x,window_y, close=['interpolate','ffill'], vol=['interpolate','fill0']):
     df = pd.read_csv(path)
     df['date'] = df.date.apply(pd.Timestamp)
     df['dow'] = df.date.apply(lambda x: x.dayofweek)
@@ -42,7 +42,6 @@ def prepair_data(path, marketcap, last_date, n,window_x,window_y, close= ['inter
     df = df[(df.dow<=4)&(df.dow>=0)]
     df = df.drop(['dow'],axis=1)
     df = df.pivot_table(index='date', columns='ticker')
-
     ## select tickers not nan in final day
 #     columns = df.close.columns[~df.close.iloc[-1].isna()]
 # select tickers in top_n marketcap
@@ -56,7 +55,6 @@ def prepair_data(path, marketcap, last_date, n,window_x,window_y, close= ['inter
             df.close = df.close.ffill()
         else:
           print('Error: Please enter the correct method of fill missing data')
-    
     for m in vol:
         if (m =='interpolate'):
             df.volume = df.volume.interpolate(method='linear',limit_area='inside',limit_direction='both', axis=0)
@@ -64,31 +62,23 @@ def prepair_data(path, marketcap, last_date, n,window_x,window_y, close= ['inter
             df.volume = df.volume.fillna(0)
         else:
           print('Error: Please enter the correct method of fill missing data')
-
     close = df.close
     daily_return = ((close.shift(-1) - close)/close).shift(1)
-
     tickers = df.close.columns
-
     X = df.values.reshape(df.shape[0],2,-1)
     y = daily_return.values
-
     ## fill X
     ##fill nan by 0.0
     X[np.isnan(X)] = 0.0
-
     ## fill y
     y[np.isnan(y)] = -1e2
     # y[np.isnan(y)] = 0
-
     # X1 = rolling_array(X[window_x:],stepsize=1,window=window_y)
-
     X = rolling_array(X[:-window_y],stepsize=1,window=window_x)
     y = rolling_array(y[window_x:],stepsize=1,window=window_y)
     X = np.moveaxis(X,-1,1)
     # X1 = np.moveaxis(X1,-1,1)
     y = np.swapaxes(y,1,2)
-
     return X,y,tickers
 
 
