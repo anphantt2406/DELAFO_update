56c7f8604855d74e8335f2e983a66bc98cdad664
Phan Thị Thùy An <73625290+anphantt2406@users.noreply.github.com>
2023-05-30 12:44:26 +0700

    Update main.py EMA20

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class DELAFO:
 		self.l2, self.l2_1, self.l2_2, self.units= l2,l2_1,l2_2, units
 	@classmethod
 	def from_existing_config(cls,path_data,path_marketcap,last_date, top_n, model_name,alpha = 0.5,timesteps_input=64,timesteps_output=19,close_fill='ffill',vol_fill='fill0',n_fold=10,batch_size=128,epochs=300,activation="sigmoid",l2=0.001,l2_1=0.01,l2_2= 0.01,units=32):
-		X,y,tickers = prepair_data(path_data, path_marketcap, last_date,n=top_n,window_x=timesteps_input,window_y=timesteps_output, close=close_fill, vol=vol_fill, periods = 5)
+		X,y,tickers = prepair_data(path_data, path_marketcap, last_date,n=top_n,window_x=timesteps_input,window_y=timesteps_output, close=close_fill, vol=vol_fill, periods = 20)
 		if model_name == "GRU":
 			hyper_params = {"activation": activation,
 					"l2": l2,
